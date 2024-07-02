25ebc9c2bf49ff4f7e0a0749f1792ad2019783df
moemeylane <wangarimaureen668@gmail.com>
2024-07-02 04:13:57 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
